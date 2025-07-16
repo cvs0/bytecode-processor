b9c673467800fc93a37f8b260c09634b4e186371
cvs0 <cvs0@lockscript.dev>
2025-07-15 21:44:28 -0600

    fix module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,4 +2,13 @@ module bytecode.processor {
     requires org.objectweb.asm.tree;
 
     exports net.cvs0.bytecode;
+    exports net.cvs0.bytecode.attribute;
+    exports net.cvs0.bytecode.member;
+    exports net.cvs0.bytecode.analysis;
+    exports net.cvs0.bytecode.util;
+    exports net.cvs0.bytecode.clazz;
+    exports net.cvs0.bytecode.instruction;
+    exports net.cvs0.bytecode.plugin;
+    exports net.cvs0.bytecode.test;
+    exports net.cvs0.bytecode.transform;
 }
